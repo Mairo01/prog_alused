c493daab074aa19c9fb7717234dfee03affd0a4e
Mairo01 <71321098+Mairo01@users.noreply.github.com>
2020-10-01 13:19:29 +0100

    Update 6.3 Kuupäev.py

--- 6.3 Kuupäev.py
+++ 6.3 Kuupäev.py
@@ -1,2 +1,6 @@
 kuud=['Jaanuar','Veebruar','Marts','Aprill','Mai','Juuni','Juuli','august','september','oktoober','november','detsember']
-kuupaev=input('Sisesta kuupaev kujul DD.MM.YYYY:')
+kp=str(input('Sisesta kujul DD.MM.YYYY: '))
+l= kp.split(".")
+kuuNR=int(l[1])
+kuu = kuud[kuuNR]
+print(l[0] +". " + kuu + " " + l[2])
